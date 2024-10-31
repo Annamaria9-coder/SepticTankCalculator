eaa6d91774f6c9ca1730093b9d7093264e863df8
Anna-maria G <67832384+Annamaria9-coder@users.noreply.github.com>
2024-10-31 16:44:21 -0700

    Update calculations.py
    This calculates volumes

--- app/calculations.py
+++ app/calculations.py
@@ -1,1 +1,12 @@
+# calculations.py
 
+from constants import SLUDGE_ACCUMULATION_RATE, FLUSH_COUNT, CISTERN_SIZE, LIQUID_DEPTH, FREEBOARD
+
+def calculate_volume_liquid(household_size):
+    return FLUSH_COUNT * CISTERN_SIZE * household_size
+
+def calculate_volume_sludge(household_size):
+    return SLUDGE_ACCUMULATION_RATE * household_size
+
+def calculate_total_volume(volume_liquid, volume_sludge):
+    return volume_liquid + volume_sludge
