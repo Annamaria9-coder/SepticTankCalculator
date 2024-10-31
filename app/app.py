2e7d770a988df32192100c1c01b9f1973f5afcc1
Anna-maria G <67832384+Annamaria9-coder@users.noreply.github.com>
2024-10-31 16:43:21 -0700

    Update app.py
    Defined the constants

--- app/app.py
+++ app/app.py
@@ -1,1 +1,7 @@
+# constants.py
 
+SLUDGE_ACCUMULATION_RATE = 0.04  # m3 per capita per year
+FLUSH_COUNT = 5  # Flush count per person
+CISTERN_SIZE = 0.009  # m3
+LIQUID_DEPTH = 1.0  # Liquid depth in meters
+FREEBOARD = 0.3  # Freeboard in meters for safety
