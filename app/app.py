4ce383d662e92554ac2072c20ada0dc4e8e97891
Anna-maria G <67832384+Annamaria9-coder@users.noreply.github.com>
2024-10-31 20:48:24 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, request
 from app.input_parameters import get_user_inputs
-from app.calculations import calculate_volume_liquid, calculate_volume_sludge, calculate_total_volume
+from app.calculations import calculate_tank_requirements
 
 app = Flask(__name__)
 
@@ -12,15 +12,17 @@ def index():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    household_size = int(request.form['household_size'])
-    tank_type = request.form['tank_type']
+    user_inputs = {
+        "household_size": int(request.form['household_size']),
+        "tank_type": request.form['tank_type'],
+        "retention_time": float(request.form['retention_time']),
+        "borehole_distance": float(request.form['borehole_distance']),
+        "seasonal_factor": float(request.form['seasonal_factor']),
+        "sand_thickness": float(request.form['sand_thickness'])
+    }
     
-    # Get calculation values
-    volume_liquid = calculate_volume_liquid(household_size)
-    volume_sludge = calculate_volume_sludge(household_size)
-    total_volume = calculate_total_volume(volume_liquid, volume_sludge)
-
-    return render_template('result.html', volume_liquid=volume_liquid, volume_sludge=volume_sludge, total_volume=total_volume)
+    results = calculate_tank_requirements(user_inputs)
+    return render_template('result.html', results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
